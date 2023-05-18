76dccd5eed3a5a1d306c79ff2b8c3e44a29670ff
Ruslan <119488421+Rusik-best86@users.noreply.github.com>
2023-05-18 19:20:12 +0300

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -15,7 +15,7 @@ const headerLinks = document.querySelectorAll('.header__menu')
 headerLinks.forEach((el) => {
    el.addEventListener('click', () => {
       // $('.header__burger,.header__menu').toggleClass('open-menu');
-      $('body').toggleClass('lock');
+      $('body').removeClass('lock');
    })
 })
 
@@ -55,116 +55,7 @@ function mainMenu(event) {
    }
 
 }
-// ............................sliders...........................
 
-new Swiper('.header__slider', {
-   slidesPerView: 1,
-   autowidth: true,
-   navigation: {
-      nextEl: '.header__slider__next',
-      prevEl: '.header__slider__prev'
-   },
-   nested: true,
-   speed: 600,
-   // loop: true,
-});
-
-new Swiper('.projects__slider', {
-   spaceBetween: 30,
-   // slidesPerView: 4,
-   autowidth: true,
-   nested: true,
-   speed: 600,
-   // loop: true,
-   pagination: {
-      el: '.projects__slider-pagination',
-      clickable: true,
-   },
-   breakpoints: {
-      // when window width is >= 320px
-      320: {
-         slidesPerView: 1,
-         spaceBetween: 10
-      },
-      // when window width is >= 480px
-      480: {
-         slidesPerView: 2,
-         spaceBetween: 10
-      },
-      768: {
-         slidesPerView: 3,
-         spaceBetween: 20
-      },
-      // when window width is >= 640px
-
-      1199: {
-         slidesPerView: 4,
-         spaceBetween: 30
-      }
-   }
-});
-
-new Swiper('.articles__slider', {
-   spaceBetween: 50,
-   slidesPerView: 3,
-   autowidth: true,
-   nested: true,
-   speed: 600,
-   // loop: true,
-   pagination: {
-      el: '.articles__slider-pagination',
-      clickable: true,
-   },
-   breakpoints: {
-      // when window width is >= 320px
-      320: {
-         slidesPerView: 1,
-         spaceBetween: 10
-      },
-      // when window width is >= 480px
-      550: {
-         slidesPerView: 2,
-         spaceBetween: 10
-      },
-      991: {
-         slidesPerView: 3,
-         spaceBetween: 20
-      },
-      // when window width is >= 640px
-   }
-});
-
-// ...........................tabs.................................................
-
-$(document).ready(function () {
-   $('.header-tabs-link').click(function (e) {
-      e.preventDefault();
-      $('.header-tabs-link').removeClass('header-tabs-link--active');
-      $('.header-tabs-block').removeClass('header-tabs-block--active');
-      $(this).addClass('header-tabs-link--active');
-      $($(this).attr('href')).addClass('header-tabs-block--active')
-
-   });
-   $('.header-tabs-link:first').click();
-});
-// ......................project
-$(document).ready(function () {
-   $('.projects__tabs_link').click(function (e) {
-      e.preventDefault();
-      $('.projects__tabs_link').removeClass('projects__tabs_link--active1');
-      $('.projects__tabs_block').removeClass('projects__tabs_block--active1');
-      $(this).addClass('projects__tabs_link--active1');
-      $($(this).attr('href')).addClass('projects__tabs_block--active1')
-
-   });
-   $('.projects__tabs_link:first').click();
-});
-
-
-
-const headerSubtitle = document.querySelector('.header-slide__subtitle');
-const headerSubtitleTwo = document.querySelector('#sub2');
-const headerSubtitleThree = document.querySelector('#sub3');
 const headerButton = document.querySelector('#popup__open');
 const headerButtonHid = document.querySelector('.header__button-hid');
 const myPopup = document.querySelector('.my__popup');
@@ -172,10 +63,6 @@ const close = document.querySelector('.popup__close');
 const closeArea = document.querySelector('.popup__area');
 const wrapper = document.querySelector('.wrapper');
 const menuList = document.querySelector('#list');
-
-// headerSubtitle.addEventListener('click', OpenPopup);
-headerSubtitleTwo.addEventListener('click', OpenPopup);
-headerSubtitleThree.addEventListener('click', OpenPopup);
 headerButton.addEventListener('click', OpenPopup);
 function OpenPopup(event) {
    myPopup.classList.toggle('open');
@@ -196,22 +83,6 @@ headerButtonHid.addEventListener('click', function (event) {
 
 })
 
-
-// спойлер-аккордион 
-$(document).ready(function () {
-   // ссылка, на которую нажимаем 
-   $('.spoiler-faq__link').click(function (event) {
-      // общий класс спойлера
-      if ($('.faq__spoiler').hasClass('one')) {
-         // ссылка, на которую нажимаем 
-         $('.spoiler-faq__link').not($(this)).removeClass('hidden');
-         // блок который идет сразу после ссылки
-         $('.spoiler-faq__block').not($(this).next()).slideUp(300);
-      }
-      $(this).toggleClass('hidden').next().slideToggle(400);
-   });
-});
-
 // footer-spoiler
 $(document).ready(function () {
    // ссылка, на которую нажимаем 
@@ -227,3 +98,50 @@ $(document).ready(function () {
    });
 });
 
+
+// счетчик цифр
+var time = 2, cc = 1;
+$(window).scroll(function () {
+   $('#counter').each(function () {
+      var
+         cPos = $(this).offset().top,
+         topWindow = $(window).scrollTop();
+      if (cPos < topWindow + 500) {
+         if (cc < 2) {
+            $('div').each(function () {
+               var
+                  i = 1,
+                  num = $(this).data('num'),
+                  step = 1000 * time / num,
+                  that = $(this),
+                  int = setInterval(function () {
+                     if (i <= num) {
+                        that.html(i);
+                     }
+                     else {
+                        cc = cc + 2;
+                        clearInterval(int);
+                     }
+                     i++;
+                  }, step);
+            });
+         }
+      }
+   });
+});
+
+
+// table-spoiler
+$(document).ready(function () {
+   // ссылка, на которую нажимаем 
+   $('.spoiler-table__link').click(function (event) {
+      // общий класс спойлера
+      if ($('.spoiler-table').hasClass('one')) {
+         // ссылка, на которую нажимаем 
+         $('.spoiler-table__link').not($(this)).removeClass('hidden');
+         // блок который идет сразу после ссылки
+         $('.spoiler-table__block').not($(this).next()).slideUp(300);
+      }
+      $(this).toggleClass('hidden').next().slideToggle(400);
+   });
+});
